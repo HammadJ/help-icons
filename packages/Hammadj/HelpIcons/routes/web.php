c339c35f09cf6e189f85946be0ad454d9022e892
HammadJ1 <hammadjavaid2@gmail.com>
2024-10-23 16:07:49 +0500

    .

--- packages/Hammadj/HelpIcons/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-use Hammadj\HelpIcons\Http\Controllers\HelpController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/help', [HelpController::class, 'index']);
-Route::get('/help/create', [HelpController::class, 'create']);
-Route::post('/help/clear-cache/{formId}', [HelpController::class, 'clearCache']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
-<?php
+<?php 
 
+use Hammadj\HelpIcons\Http\Controllers\HelpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/help', [HelpController::class, 'index']);
+Route::get('/help/create', [HelpController::class, 'create']);
+Route::post('/help/clear-cache/{formId}', [HelpController::class, 'clearCache']);
